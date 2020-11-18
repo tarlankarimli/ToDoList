e91180db6a1183e371eea4ae4a6a6668389cf923
Tarlan Karimli <terlankerimli@gmail.com>
2020-11-19 00:20:24 +0400

    addeventlistener problem

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let TaskData = []; 
- 
+let TaskData = [];
+
 // buttons 
 const addBtn = document.querySelector('.fa-plus-circle');
 const editBtn = document.querySelector('.fa-edit');
@@ -15,46 +15,60 @@ let taskID = -1;
 let waitedContent = document.querySelector('.waited-content');
 
 // Display task modal 
-addBtn.addEventListener("click", ()=> {
+addBtn.addEventListener("click", () => {
     document.querySelector('.task-modal').style.display = "flex";
 })
 // Push new tasks to TaskData
 uploadBtn.addEventListener("click", () => {
     let content = newTask.value;
-    taskID ++;
-    TaskData.push({id: taskID, content: content, completed: false})
-    newTask.value ="";
-    createTaskNode();
-    document.querySelector('.task-modal').style.display = "none";    
+    taskID++;
+    TaskData.push({ id: taskID, content: content, completed: false })
+    newTask.value = "";
+    waitedContent.innerHTML = "";
+    listTasks();
+    
+    document.querySelector('.task-modal').style.display = "none";
 })
-// Create html nodes to display task
-const createTaskNode = () => {
-let editIcon = document.createElement("i");
-editIcon.className = "fas fa-edit";
 
-let deleteIcon = document.createElement("i");
-deleteIcon.className = "fas fa-toilet";
-
-let checkIcon = document.createElement("i");
-checkIcon.className = "far fa-square";
-
-let waitedTaskBtns = document.createElement("div");
-waitedTaskBtns.className = "waited-task-btns";
-
-let listTask = document.createElement("div");
-listTask.className = "list-task";
-let textNode = document.createTextNode(TaskData[taskID].content);
-listTask.appendChild(textNode);
-listTask.setAttribute("id", taskID)
-// listTask.setAttribute("draggable", true);
-
-let waitedTask = document.createElement("div");
-waitedTask.className = "waited-task";
-
-waitedTaskBtns.appendChild(editIcon);
-waitedTaskBtns.appendChild(deleteIcon);
-waitedTaskBtns.appendChild(checkIcon);
-waitedTask.appendChild(listTask);
-waitedTask.appendChild(waitedTaskBtns);
-waitedContent.appendChild(waitedTask);
+const listTasks = () => {
+    for (let id = 0; id < TaskData.length; id++) {
+        createTaskNode(id)
+    }
 }
+// Create html nodes to display task
+const createTaskNode = (id) => {
+    let editIcon = document.createElement("i");
+    editIcon.className = "fas fa-edit";
+
+    let deleteIcon = document.createElement("i");
+    deleteIcon.className = "fas fa-toilet";
+
+    let checkIcon = document.createElement("i");
+    checkIcon.className = "far fa-square";
+
+    let waitedTaskBtns = document.createElement("div");
+    waitedTaskBtns.className = "waited-task-btns";
+
+    let listTask = document.createElement("div");
+    listTask.className = "list-task";
+    let textNode = document.createTextNode(TaskData[id].content);
+    listTask.appendChild(textNode);
+    listTask.setAttribute("id", id)
+    // listTask.setAttribute("draggable", true);
+
+    let waitedTask = document.createElement("div");
+    waitedTask.className = "waited-task";
+
+    waitedTaskBtns.appendChild(editIcon);
+    waitedTaskBtns.appendChild(deleteIcon);
+    waitedTaskBtns.appendChild(checkIcon);
+    waitedTask.appendChild(listTask);
+    waitedTask.appendChild(waitedTaskBtns);
+    waitedContent.appendChild(waitedTask);
+}
+
+    // let listTask = document.querySelector('.list-task');
+    // deleteBtn.addEventListener("onclick", (e)=> {
+    //     let test = e.target
+        
+    // } )
