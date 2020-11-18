bb9bc2ee3ff77b0dced6113edbbeb19d16a5d909
Tarlan Karimli <terlankerimli@gmail.com>
2020-11-18 23:13:44 +0400

    failed drag drop

---
+++ app.js
@@ -0,0 +1,60 @@
+let TaskData = []; 
+ 
+// buttons 
+const addBtn = document.querySelector('.fa-plus-circle');
+const editBtn = document.querySelector('.fa-edit');
+const deleteBtn = document.querySelector('.fa-toilet');
+const uncheckBtn = document.querySelector('.fa-square');
+const sliderBtn = document.querySelector('.fa-chevron-right');
+const uploadBtn = document.querySelector('.fa-arrow-circle-up');
+let newTask = document.querySelector('#new-task');
+
+// start valued variables
+let taskID = -1;
+
+let waitedContent = document.querySelector('.waited-content');
+
+// Display task modal 
+addBtn.addEventListener("click", ()=> {
+    document.querySelector('.task-modal').style.display = "flex";
+})
+// Push new tasks to TaskData
+uploadBtn.addEventListener("click", () => {
+    let content = newTask.value;
+    taskID ++;
+    TaskData.push({id: taskID, content: content, completed: false})
+    newTask.value ="";
+    createTaskNode();
+    document.querySelector('.task-modal').style.display = "none";    
+})
+// Create html nodes to display task
+const createTaskNode = () => {
+let editIcon = document.createElement("i");
+editIcon.className = "fas fa-edit";
+
+let deleteIcon = document.createElement("i");
+deleteIcon.className = "fas fa-toilet";
+
+let checkIcon = document.createElement("i");
+checkIcon.className = "far fa-square";
+
+let waitedTaskBtns = document.createElement("div");
+waitedTaskBtns.className = "waited-task-btns";
+
+let listTask = document.createElement("div");
+listTask.className = "list-task";
+let textNode = document.createTextNode(TaskData[taskID].content);
+listTask.appendChild(textNode);
+listTask.setAttribute("id", taskID)
+// listTask.setAttribute("draggable", true);
+
+let waitedTask = document.createElement("div");
+waitedTask.className = "waited-task";
+
+waitedTaskBtns.appendChild(editIcon);
+waitedTaskBtns.appendChild(deleteIcon);
+waitedTaskBtns.appendChild(checkIcon);
+waitedTask.appendChild(listTask);
+waitedTask.appendChild(waitedTaskBtns);
+waitedContent.appendChild(waitedTask);
+}
---
+++ banner-data.js
@@ -0,0 +1,18 @@
+const bannerData = [
+    "Bugünün işini sabaha qoyma",
+    "Adam ol!",
+    "Başını buraxma"
+]
+
+let bannerContent = document.querySelector('.banner-content')
+let i = 0;
+setInterval(() => {
+    let textNode = document.createTextNode(bannerData[i]);
+    bannerContent.appendChild(textNode);
+    i<bannerData.length-1 ? i++ : i=0;
+    setTimeout(() => {
+        bannerContent.removeChild(textNode)
+    }, 2000);
+}, 3000);
+
+
